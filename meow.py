f9f8818de80eb1280bdb4d8f5622d182b38891b3
sasha <sasha.govorova100@gmail.com>
2022-10-23 14:37:35 +0300

    2

--- meow.py
+++ meow.py
@@ -2,3 +2,17 @@ import random
 
 def game(choice, result):
     computer_choice = random.choice("rps")
+    if str.lower(choice) == computer_choice:
+        print("Draw")
+    elif str.lower(choice) == "r" and computer_choice == "p":
+        result["computer"]+=1
+    elif str.lower(choice) == "r" and computer_choice == "s":
+        result["player"]+=1
+    elif str.lower(choice) == "p" and computer_choice == "s":
+        result["computer"] += 1
+    elif str.lower(choice) == "p" and computer_choice == "r":
+        result["player"] += 1
+    elif str.lower(choice) == "s" and computer_choice == "r":
+        result["computer"] += 1
+    elif str.lower(choice) == "s" and computer_choice == "p":
+        result["player"] += 1
