ddfba450808c0ace7725bcde151bb77463134184
sasha <sasha.govorova100@gmail.com>
2022-10-23 14:35:40 +0300

    11

--- meow.py
+++ meow.py
@@ -1,6 +1,4 @@
 import random
+
 def game(choice, result):
-    print("")
-    print("=====Start Game rock, paper,  scissors=====")
     computer_choice = random.choice("rps")
-    print("--------------------------------")
